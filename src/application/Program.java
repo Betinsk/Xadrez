fb9091c6e619050450e8cb69eeafe1a781098586
Betinsk <matheusf730@gmail.com>
2026-02-05 20:10:06 -0300

    PrintBoard

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,18 @@
 package application;
 
 import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Board board = new Board(8, 8);
-		System.out.println(board);
+	
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }
